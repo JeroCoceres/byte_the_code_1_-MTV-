7bbedd554d12e4a5d27ec1cae08116b7b5c7df61
Jeronimo Coceres <jeronimococeres@gmail.com>
2023-03-05 15:24:30 -0300

    primer commit

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+from users.views import login_view
+
+
+urlpatterns = [
+    path("login/",login_view, name="login"),
+    path("logout/",LogoutView.as_view(template_name = "users/login.html"), name="logout")
+
+]
