b08c35ee538a30d8e496cca60d86ca208a5ae871
Jeronimo Coceres 2 <jeronimococeres@gmail.com>
2023-03-05 18:18:28 -0300

    userlist

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login
 from django.contrib.auth.forms import AuthenticationForm
+from users.forms import RegisterForm
 
 def login_view(request):
     if request.method == "GET":
@@ -20,3 +21,20 @@ def login_view(request):
                 "errors":form.errors,
             }
             return render(request,"users/login.html",context)
+
+def register(request):
+    if request.method == "GET":
+        return render(request, "users/register.html")
+    elif request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect ("index")
+        else:
+            context = {
+                "errors" : form.errors,
+            }
+            return render(request,"users/register.html",context)
+        
+def users_list_view(request):
+    return render (request," users/users_list.html")
